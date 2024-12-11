5fb402021d96afdb79b3e93927f42746377f0b6e
adam.paslawski <adampaslawski@morningstar.com>
2024-12-11 15:52:33 -0500

    day 11 do a little checking of how sparse the nubmer space is of  the tree, looks like all paths produced some sort of cycle past a certain depth

--- day11/solution1.py
+++ day11/solution1.py
@@ -1,11 +1,10 @@
 from math import log10, ceil
-from functools import cache
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(1500)
 
 input = [int(num) for num in "17639 47 3858 0 470624 9467423 5 188".split(" ")]
 
-print(f"input: {input}")
-
-@cache
 def num_digits(num: int):
 
     num_digits = ceil(log10(num + 1))
@@ -28,7 +27,6 @@ def split_int_down_middle(num: int):
 
 
 
-@cache
 def apply_rules(num):
 
     if num == 0:
@@ -38,16 +36,22 @@ def apply_rules(num):
     else:
         return [num*2024]
 
-
-@cache
+manual_cache = {}
 def expand_num(num: int, depth: int) -> int:
+
+    if (num,depth) in manual_cache:
+        ans,hits = manual_cache[(num,depth)]
+        manual_cache[(num,depth)] = (ans,hits+1)
+        return ans
 
     if depth == 0:
         return 1
 
     res = apply_rules(num)
-    
+
+
     ans = sum([expand_num(n, depth - 1) for n in res])
+    manual_cache[(num,depth)] = (ans,1)
 
     return ans
 
@@ -70,4 +74,23 @@ for num in input:
 
 print(counter)
 
+counter = 0
+for num in input:
+    counter += expand_num(num, 300)
+print("Super high depth...")
+print(counter)
 
+
+# Pretty sure this whole thing just loops eventually... Lets see..
+hit_totals = defaultdict(int)
+for (num, depth), (ans, hits) in manual_cache.items():
+    hit_totals[(num, depth)] += hits
+hit_list = [(total_hits, depth, num) for (num, depth), total_hits in hit_totals.items()]
+# Unique numbers hit
+nums = set([num for (num, depth), total_hits in hit_totals.items()])
+
+#cache hit count high to low by number of hits
+# I guess the hypothesis is it's some cycle inlcuding alot of 0->1->2024->...->0->1 you get the idea.
+sorted_hit_list = sorted(hit_list, key=lambda x: x[2])
+
+print(f"Total unique numbers {len(nums)}")
